a340cda80e8c2626f502df0fa3aeb3d9410edf02
drashtikothi729-stack <drashtikothi729@gmail.com>
2026-09-24 14:14:38 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,3 @@
+celcius=int(input("Enter Temperature in Celcius:"))
+result1=celcius*1.8+32
+print("The temperature in fehrenheit is:",result1)
---
+++ p7.py
@@ -0,0 +1,9 @@
+input1=int(input("Physics:"))
+input2=int(input("Chemistry:"))
+input3=int(input("Mathematics:"))
+input4=int(input("English:"))
+input5=int(input("CS:"))
+result1=input1+input2+input3+input4+input5
+result2=(input1+input2+input3+input4+input5)/5
+print("The total is:",result1)
+print("The percentage is:",result2)
